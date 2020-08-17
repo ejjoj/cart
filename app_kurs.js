66999ed0b855b35248d668d7264c01942592d114
ejjoj <szymaskibartosz@gmail.com>
2020-08-17 18:13:38 +0200

    Inline styles added conditionally

--- app_kurs.js
+++ app_kurs.js
@@ -25,10 +25,11 @@ class App extends React.Component {
   }
 
   render() {
+    const style = this.state.shoppingCart === 0 ? {opacity: 0.3} : {};
     return (
       <div>
         <button disabled={!this.state.shoppingCart} onClick={this.handleRemoveFromCart}>-</button>
-        <span> {this.state.shoppingCart} </span>
+        <span style={style}> {this.state.shoppingCart} </span>
         <button disabled={this.state.shoppingCart === this.state.availableProducts} onClick={this.handleAddToCart}>+</button>
         {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
       </div>
